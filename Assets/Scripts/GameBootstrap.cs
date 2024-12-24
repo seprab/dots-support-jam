6f7e9648520999b9a617a42314fff92bde263c4b
Alejandro Lopez <alejandro@unity3d.com>
2024-12-24 11:58:22 -0500

    Adding netcode
    adding bootstrap

---
+++ Assets/Scripts/GameBootstrap.cs
@@ -0,0 +1,12 @@
+using Unity.NetCode;
+using UnityEngine;
+
+[UnityEngine.Scripting.Preserve]
+public class GameBootstrap : ClientServerBootstrap
+{
+    public override bool Initialize(string defaultWorldName)
+    {
+        AutoConnectPort = 7979;
+        return base.Initialize(defaultWorldName);
+    }
+}
