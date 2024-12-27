0b9020bf9715a03716e1d07f451a569dc8873dd3
seprab <sergio.prada@unity3d.com>
2024-12-27 12:35:23 -0500

    making player components ghost components to sync with server

--- Assets/Scripts/Components/Health.cs
+++ Assets/Scripts/Components/Health.cs
@@ -1,5 +1,7 @@
 using Unity.Entities;
+using Unity.NetCode;
 
+[GhostComponent]
 public struct Health : IComponentData
 {
     public float Value;
--- Assets/Scripts/Components/Speed.cs
+++ Assets/Scripts/Components/Speed.cs
@@ -1,5 +1,7 @@
 using Unity.Entities;
+using Unity.NetCode;
 
+[GhostComponent]
 public struct Speed : IComponentData
 {
     public float Value;
