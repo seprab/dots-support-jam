0088ffa8901a771a3562db2e19cba4e1e47967f3
Alejandro Lopez <alejandro@unity3d.com>
2025-01-09 14:45:03 -0500

    fixing join game

--- Assets/Scripts/Monobehaviour/UI/FrontendHUD.cs
+++ Assets/Scripts/Monobehaviour/UI/FrontendHUD.cs
@@ -5,92 +5,91 @@ using Unity.NetCode;
 using System.Collections.Generic;
 
 
-    public class FrontendHUD : MonoBehaviour
+public class FrontendHUD : MonoBehaviour
+{
+    [SerializeField] UnityEngine.EventSystems.EventSystem m_EventSystem;
+
+    public string ConnectionStatus
     {
-        [SerializeField]
-        UnityEngine.EventSystems.EventSystem m_EventSystem;
-
-        public string ConnectionStatus
-        {
-            get { return m_ConnectionLabel.text; }
-            set { m_ConnectionLabel.text = value; }
-        }
-
-        [SerializeField]
-        UnityEngine.UI.Text m_ConnectionLabel;
-
-        public void ReturnToFrontend()
-        {
-            Debug.Log("[ReturnToFrontend] Called.");
-            var clientServerWorlds = new List<World>();
-            foreach (var world in World.All)
-            {
-                if (world.IsClient() || world.IsServer())
-                    clientServerWorlds.Add(world);
-            }
-
-            foreach (var world in clientServerWorlds)
-                world.Dispose();
-
-            //if (string.IsNullOrEmpty(Frontend.OldFrontendWorldName))
-              //  Frontend.OldFrontendWorldName = "DefaultWorld";
-            //ClientServerBootstrap.CreateLocalWorld(Frontend.OldFrontendWorldName);
-            
-            ClientServerBootstrap.CreateLocalWorld("DefaultWorld");
-            
-            SceneManager.LoadScene("Frontend", LoadSceneMode.Single);
-        }
-
-        public void Start()
-        {
-            if (ClientServerBootstrap.ClientWorld != null)
-            {
-                var sys = ClientServerBootstrap.ClientWorld.GetOrCreateSystemManaged<FrontendHUDSystem>();
-                sys.UIBehaviour = this;
-                var simGroup = ClientServerBootstrap.ClientWorld.GetExistingSystemManaged<SimulationSystemGroup>();
-                simGroup.AddSystemToUpdateList(sys);
-            }
-
-            // We must always have an event system (DOTS-7177), but some scenes will already have one,
-            // so we only enable ours if we can't find someone else's.
-            if (Object.FindFirstObjectByType<UnityEngine.EventSystems.EventSystem>() == null)
-                m_EventSystem.gameObject.SetActive(true);
-        }
+        get { return m_ConnectionLabel.text; }
+        set { m_ConnectionLabel.text = value; }
     }
 
-    [WorldSystemFilter(WorldSystemFilterFlags.ClientSimulation)]
-    [UpdateInGroup(typeof(LateSimulationSystemGroup))]
-    [DisableAutoCreation]
-    public partial class FrontendHUDSystem : SystemBase
-    {
-        public FrontendHUD UIBehaviour;
-        string m_PingText;
+    [SerializeField] UnityEngine.UI.Text m_ConnectionLabel;
 
-        protected override void OnUpdate()
+    public void ReturnToFrontend()
+    {
+        Debug.Log("[ReturnToFrontend] Called.");
+        var clientServerWorlds = new List<World>();
+        foreach (var world in World.All)
         {
-            CompleteDependency();
-            if (!SystemAPI.TryGetSingletonEntity<NetworkStreamConnection>(out var connectionEntity))
+            if (world.IsClient() || world.IsServer())
+                clientServerWorlds.Add(world);
+        }
+
+        foreach (var world in clientServerWorlds)
+            world.Dispose();
+
+        //if (string.IsNullOrEmpty(Frontend.OldFrontendWorldName))
+        //  Frontend.OldFrontendWorldName = "DefaultWorld";
+        //ClientServerBootstrap.CreateLocalWorld(Frontend.OldFrontendWorldName);
+
+        ClientServerBootstrap.CreateLocalWorld("DefaultWorld");
+
+        SceneManager.LoadScene("Frontend", LoadSceneMode.Single);
+    }
+
+    public void Start()
+    {
+        if (ClientServerBootstrap.ClientWorld != null)
+        {
+            var sys = ClientServerBootstrap.ClientWorld.GetOrCreateSystemManaged<FrontendHUDSystem>();
+            sys.UIBehaviour = this;
+            var simGroup = ClientServerBootstrap.ClientWorld.GetExistingSystemManaged<SimulationSystemGroup>();
+            simGroup.AddSystemToUpdateList(sys);
+        }
+
+        // We must always have an event system (DOTS-7177), but some scenes will already have one,
+        // so we only enable ours if we can't find someone else's.
+        if (Object.FindFirstObjectByType<UnityEngine.EventSystems.EventSystem>() == null)
+            m_EventSystem.gameObject.SetActive(true);
+    }
+}
+
+[WorldSystemFilter(WorldSystemFilterFlags.ClientSimulation)]
+[UpdateInGroup(typeof(LateSimulationSystemGroup))]
+[DisableAutoCreation]
+public partial class FrontendHUDSystem : SystemBase
+{
+    public FrontendHUD UIBehaviour;
+    string m_PingText;
+
+    protected override void OnUpdate()
+    {
+        CompleteDependency();
+        if (!SystemAPI.TryGetSingletonEntity<NetworkStreamConnection>(out var connectionEntity))
+        {
+            UIBehaviour.ConnectionStatus = "Not connected!";
+            m_PingText = default;
+        }
+        else
+        {
+            var connection = EntityManager.GetComponentData<NetworkStreamConnection>(connectionEntity);
+            var address = SystemAPI.GetSingletonRW<NetworkStreamDriver>().ValueRO.GetRemoteEndPoint(connection).Address;
+            if (EntityManager.HasComponent<NetworkId>(connectionEntity))
             {
-                UIBehaviour.ConnectionStatus = "Not connected!";
-                m_PingText = default;
+                if (string.IsNullOrEmpty(m_PingText) || UnityEngine.Time.frameCount % 30 == 0)
+                {
+                    var networkSnapshotAck = EntityManager.GetComponentData<NetworkSnapshotAck>(connectionEntity);
+                    m_PingText = networkSnapshotAck.EstimatedRTT > 0
+                        ? $"{(int)networkSnapshotAck.EstimatedRTT}ms"
+                        : "Connected";
+                }
+
+                UIBehaviour.ConnectionStatus = $"{address} | {m_PingText}";
             }
             else
-            {
-                var connection = EntityManager.GetComponentData<NetworkStreamConnection>(connectionEntity);
-                var address = SystemAPI.GetSingletonRW<NetworkStreamDriver>().ValueRO.GetRemoteEndPoint(connection).Address;
-                if (EntityManager.HasComponent<NetworkId>(connectionEntity))
-                {
-                    if (string.IsNullOrEmpty(m_PingText) || UnityEngine.Time.frameCount % 30 == 0)
-                    {
-                        var networkSnapshotAck = EntityManager.GetComponentData<NetworkSnapshotAck>(connectionEntity);
-                        m_PingText = networkSnapshotAck.EstimatedRTT > 0 ? $"{(int)networkSnapshotAck.EstimatedRTT}ms" : "Connected";
-                    }
-
-                    UIBehaviour.ConnectionStatus = $"{address} | {m_PingText}";
-                }
-                else
-                    UIBehaviour.ConnectionStatus = $"{address} | Connecting";
-            }
+                UIBehaviour.ConnectionStatus = $"{address} | Connecting";
         }
     }
-
+}
--- Assets/Scripts/Monobehaviour/UI/MainMenu.cs
+++ Assets/Scripts/Monobehaviour/UI/MainMenu.cs
@@ -9,19 +9,23 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private  UnityEngine.UI.Button _startServerButton;
-    [SerializeField] private  UnityEngine.UI.Button _joinServerButton;
-    [SerializeField] private  UnityEngine.UI.Button _quitButton;
+    [SerializeField] private UnityEngine.UI.Button _startServerButton;
+    [SerializeField] private UnityEngine.UI.Button _joinServerButton;
+
+    [SerializeField] private UnityEngine.UI.Button _quitButton;
+
     //[SerializeField] private SceneAsset _entitiesSubscene;
     [SerializeField] private SceneAsset sceneName;
     public Text address;
-    public Text port;    
+    public Text port;
+
     private void Start()
     {
         _startServerButton.onClick.AddListener(StartServer);
         _joinServerButton.onClick.AddListener(JoinServer);
         //_quitButton.onClick.AddListener(Quit);
     }
+
     private void StartServer()
     {
         Debug.Log("Start Server");
@@ -42,33 +46,33 @@ public class MainMenu : MonoBehaviour
             World.DefaultGameObjectInjectionWorld = serverWorld;
         }
         //SceneManager.LoadSceneAsync(_entitiesSubscene.name, LoadSceneMode.Additive);
-        
+
         SceneManager.LoadScene("FrontendHUD");
 
         //Destroy the local simulation world to avoid the game scene to be loaded into it
         //This prevent rendering (rendering from multiple world with presentation is not greatly supported)
         //and other issues.
         DestroyLocalSimulationWorld();
-        
+
         //TODO  revisar esto que es copiado del ejemplo de netcode 
         if (World.DefaultGameObjectInjectionWorld == null)
             World.DefaultGameObjectInjectionWorld = serverWorld;
-        
+
         // TODO check if we need to load the subscene or the scene 
         SceneManager.LoadSceneAsync(sceneName.name, LoadSceneMode.Additive);
-        
-        
+
+
         ushort port = 7979;
         RefRW<NetworkStreamDriver> networkStreamDriver = serverWorld.EntityManager
             .CreateEntityQuery(typeof(NetworkStreamDriver)).GetSingletonRW<NetworkStreamDriver>();
         networkStreamDriver.ValueRW.Listen(NetworkEndpoint.AnyIpv4.WithPort(port));
-        
+
         NetworkEndpoint connectNetworkEndpoint = NetworkEndpoint.LoopbackIpv4.WithPort(port);
         networkStreamDriver = clientWorld.EntityManager
             .CreateEntityQuery(typeof(NetworkStreamDriver)).GetSingletonRW<NetworkStreamDriver>();
         networkStreamDriver.ValueRW.Connect(clientWorld.EntityManager, connectNetworkEndpoint);
-
     }
+
     private void JoinServer()
     {
         Debug.Log("Join Server");
@@ -87,18 +91,20 @@ public class MainMenu : MonoBehaviour
         {
             World.DefaultGameObjectInjectionWorld = clientWorld;
         }
+        SceneManager.LoadScene("FrontendHUD");
         SceneManager.LoadSceneAsync(sceneName.name, LoadSceneMode.Additive);
 
-        NetworkEndpoint connectNetworkEndpoint = NetworkEndpoint.Parse(address.text , ParsePortOrDefault(port.text) );
+        NetworkEndpoint connectNetworkEndpoint = NetworkEndpoint.Parse(address.text, ParsePortOrDefault(port.text));
         RefRW<NetworkStreamDriver> networkStreamDriver = clientWorld.EntityManager
             .CreateEntityQuery(typeof(NetworkStreamDriver)).GetSingletonRW<NetworkStreamDriver>();
         networkStreamDriver.ValueRW.Connect(clientWorld.EntityManager, connectNetworkEndpoint);
     }
+
     private void Quit()
     {
         Debug.Log("Quit");
     }
-    
+
     // Tries to parse a port, returns true if successful, otherwise false
     // The port will be set to whatever is parsed, otherwise the default port of k_NetworkPort
     private UInt16 ParsePortOrDefault(string s)
@@ -108,9 +114,10 @@ public class MainMenu : MonoBehaviour
             Debug.LogWarning($"Unable to parse port, using default port {7979}");
             return 7979;
         }
+
         return port;
     }
-    
+
     protected void DestroyLocalSimulationWorld()
     {
         foreach (var world in World.All)
